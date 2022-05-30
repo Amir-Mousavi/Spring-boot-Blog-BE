56d3af423abecca3b688246c281bdf89d64d259c
Amir Mousavi <amir@reason.al>
2022-05-31 00:39:27 +0200

    Firebase Admin SDK

---
+++ src/main/kotlin/com/tutorial/blog/service/FirebaseService.kt
@@ -0,0 +1,19 @@
+package com.tutorial.blog.service
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.springframework.stereotype.Service
+import java.io.FileInputStream
+
+@Service
+class FirebaseService {
+
+    init {
+        val builder = FirebaseOptions.builder()
+        val options: FirebaseOptions =
+            builder.setCredentials(GoogleCredentials.fromStream(FileInputStream("./src/main/resources/firebase.json")))
+                .build()
+        FirebaseApp.initializeApp(options)
+    }
+}
