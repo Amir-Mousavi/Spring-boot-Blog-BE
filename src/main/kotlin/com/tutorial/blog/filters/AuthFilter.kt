2c345e782913a6af85e7113df6e1d1af87c98190
Amir Mousavi <amir@reason.al>
2022-05-31 00:51:30 +0200

    firebase filter

---
+++ src/main/kotlin/com/tutorial/blog/filters/AuthFilter.kt
@@ -0,0 +1,27 @@
+package com.tutorial.blog.filters
+
+import com.tutorial.blog.service.FirebaseService
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class AuthFilter(val firebaseService: FirebaseService): OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val idToken = request.getHeader("Authorization")
+
+        val firebaseToken = firebaseService.getFirebaseToken(idToken)
+
+        if (firebaseToken?.uid != null) {
+            filterChain.doFilter(request, response)
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+        }
+    }
+}
--- src/main/kotlin/com/tutorial/blog/service/FirebaseService.kt
+++ src/main/kotlin/com/tutorial/blog/service/FirebaseService.kt
@@ -3,6 +3,8 @@ package com.tutorial.blog.service
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseToken
 import org.springframework.stereotype.Service
 import java.io.FileInputStream
 
@@ -15,5 +17,13 @@ class FirebaseService {
             builder.setCredentials(GoogleCredentials.fromStream(FileInputStream("./src/main/resources/firebase.json")))
                 .build()
         FirebaseApp.initializeApp(options)
+    }
+
+    fun getFirebaseToken(idToken: String?): FirebaseToken? {
+        if (idToken == null) {
+            return null
+        }
+
+        return FirebaseAuth.getInstance().verifyIdToken(idToken)
     }
 }
